5cf28bf6768daf0b543d4cde53adb2eefa330b92
Spandan Barve <spandan567@gmail.com>
2024-05-15 15:18:41 +0530

    rewamp for meme voting

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,36 +1,77 @@
 "use client";
 
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import RegisterButton from "./_components/RegisterButton";
+import PollDetail from "./polls/[id]/page";
 import type { NextPage } from "next";
-import HeroImage from "~~/assets/private_voting.png";
+import { useAccount, useNetwork } from "wagmi";
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { useAuthContext } from "~~/contexts/AuthContext";
 import { useAuthUserOnly } from "~~/hooks/useAuthUserOnly";
 
 const Home: NextPage = () => {
   useAuthUserOnly({ inverted: true });
 
+  const { address } = useAccount();
+  const { chain, chains } = useNetwork();
+
+  const { isRegistered } = useAuthContext();
+
+  const [usable, setUsable] = useState(false);
+
+  useEffect(() => {
+    if (chain && chain.id && (chain.id == chains[0].id || chain.id == chains[1].id)) {
+      setUsable(true);
+    } else {
+      setUsable(false);
+    }
+  }, [chain, address]);
+
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-4xl font-bold text-center">Private Voting Starter Kit with MACI</h1>
+      <main className="w-full h-screen bg-black relative">
+        <div className="flex flex-col gap-y-5 my-10 items-center">
+          <div className="">
+            <h1 className="text-4xl font-bold text-center">Let's settle the debate</h1>
+            <h2 className="text-3xl font-bold text-center">once and for all</h2>
+          </div>
+          {!usable && <RainbowKitCustomConnectButton />}
+          {usable && !isRegistered && <RegisterButton />}
+          {usable && isRegistered && (
+            <div className="mt-16 p-6 rounded-2xl bg-slate-800 flex flex-col items-center">
+              <h1 className="font-bold text-3xl">Which one do you use?</h1>
 
-          <div className="flex flex-col-reverse md:flex-row justify-center items-center mt-10 sm:w-2/3 mx-auto gap-x-10 gap-y-5 mb-10">
-            <div className="flex-1">
-              <p className="text-lg mt-5 text-justify">
-                This starter kit is designed to help you get started with private voting using the Minimal
-                Anti-Collusion Infrastructure (MACI).
-              </p>
-              <div className="text-center">
-                <RegisterButton />
+              <div className="mt-10 flex flex-col gap-y-6">
+                {["I use onchain", "I use on-chain"].map((item, key) => (
+                  <button
+                    key={key}
+                    className="text-left bg-black py-2 px-10 font-medium rounded-full flex items-center gap-x-4 text-xl group"
+                  >
+                    <figure className="bg-gray-500 h-[0.9em] aspect-square rounded-full outline outline-gray-400 outline-offset-2 outline-1 group-hover:bg-white duration-300 group-hover:outline-white" />
+                    {item}
+                  </button>
+                ))}
               </div>
             </div>
-            <div className="flex-1">
-              <Image src={HeroImage} alt="MACI" className="w-full" />
+          )}
+
+          <div className="mt-14 bg-slate-600 rounded-3xl flex overflow-hidden w-2/3">
+            <img className="h-[30vh] aspect-square object-cover" src="/fai.jpg" />
+            <div className="flex flex-col p-5 gap-y-3">
+              <h1 className="text-2xl font-bold">Cast your Vote securely and privately</h1>
+              <p>
+                Welcome to our privacy-focused voting platform. We prioritize your privacy and the security of your vote
+                with advanced encryption technology. Cast your vote with confidence, knowing your privacy is protected.
+              </p>
             </div>
           </div>
+
+          <div className="absolute right-6 bottom-2 flex items-center flex-col">
+            <FaucetButton />
+            <p className="text-xs mt-1">Get tokens from faucet</p>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 };
--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -28,9 +28,9 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Header />
+        {/* <Header /> */}
         <main className="relative flex flex-col flex-1">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
       <Toaster />
     </>
